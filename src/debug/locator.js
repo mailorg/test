b31d0509577275a440f1ed8ee27ac94a728e710b
lcf <lcf.vs@mailo.com>
2021-12-24 09:33:17 +0100

    removing host from debug.locator

--- src/debug/locator.js
+++ src/debug/locator.js
@@ -3,7 +3,9 @@ export default (
 ) => {
   const { pathname } = new URL(url)
 
-  return (element) => {
+  return (
+    element
+  ) => {
     console.log(`Logged from ${pathname}`, element)
   }
 }
