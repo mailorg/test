906e508c8075eeaaeab5b182f59de657b71ed669
lcf <lcf.vs@mailo.com>
2022-02-07 16:24:37 +0100

    late one()

--- src/ea/href.js
+++ src/ea/href.js
@@ -3,13 +3,13 @@ import one from '@mailobj-js/front/selectors/one.js'
 
 const { document } = defaults
 const { location } = document
-const service = one('meta[name="ea_service"]').content
-const s = one('meta[name="ea_s"]').content
 
 export default (
   path,
   params = {}
 ) => {
+  const service = one('meta[name="ea_service"]').content
+  const s = one('meta[name="ea_s"]').content
   const url = new URL(`/${service}/${path}.php`, location)
 
   url.search = `${new URLSearchParams(Object.entries({ ...params, s }))}`
