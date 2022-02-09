9c82ca60446d0514e34b9a9d9f0b1fc5d699f00b
lcf <lcf.vs@mailo.com>
2022-02-09 12:35:00 +0100

    defered codemirror

--- src/editors/codeMirror/code.js
+++ src/editors/codeMirror/code.js
@@ -1,13 +1,13 @@
 import defaults from '@mailobj-js/front/selectors/defaults.js'
+import defer from '@mailobj-js/front/tree/defer.js'
 import replaceWith from '@mailobj-js/front/tree/replaceWith.js'
 
 const { window } = defaults
 
-export default (
-  code
-) => {
-  requestAnimationFrame(() => {
-    const { CodeMirror } = window
+const task = code => {
+  const { CodeMirror } = window
+
+  if (CodeMirror) {
     const { textContent } = code
     const init = node => replaceWith(code, node)
 
@@ -18,5 +18,13 @@ export default (
       mode: 'javascript',
       value: textContent
     })
-  })
+
+    return true
+  }
+}
+
+export default (
+  code
+) => {
+  defer(code, task)
 }
--- src/editors/codeMirror/textarea.js
+++ src/editors/codeMirror/textarea.js
@@ -1,14 +1,21 @@
 import defaults from '@mailobj-js/front/selectors/defaults.js'
+import defer from '@mailobj-js/front/tree/defer.js'
 import config from './config.js'
 
 const { window } = defaults
 
+const task = textarea => {
+  const { CodeMirror } = window
+
+  if (CodeMirror) {
+    CodeMirror.fromTextArea(textarea, config)
+
+    return true
+  }
+}
+
 export default (
   textarea
 ) => {
-  requestAnimationFrame(() => {
-    const { CodeMirror } = window
-
-    CodeMirror.fromTextArea(textarea, config)
-  })
+  defer(textarea, task)
 }
