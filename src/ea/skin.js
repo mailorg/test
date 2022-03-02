9c159310a3f1a77210591d1092b8b80b69d518d6
git <ea@dev-1.localhost>
2022-03-02 15:49:52 +0100

    + optional skin

--- src/ea/skin.js
+++ src/ea/skin.js
@@ -1,7 +1,3 @@
 import one from '@mailobj-js/front/selectors/one.js'
 
-<<<<<<< HEAD
 export default one('meta[name="ea_skin"]')?.content
-=======
-export default one('meta[name="ea_skin"]').content
->>>>>>> a14ec9354a2ffc67361a340a7bf1a2b505bdd0b3
