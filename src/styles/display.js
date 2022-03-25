5ac0cd90422a411cf0e3f0d59a8cf913c01ca561
lcf <lcf.vs@mailo.com>
2022-03-25 17:37:07 +0100

    + styles

--- src/styles/display.js
+++ src/styles/display.js
@@ -1,10 +1,5 @@
 import object from '@mailobj-js/front/utils/object.js'
 
-export const classes = object(null, {
-  none: 'ea_utilities__display-none'
-})
-
 export default object(null, {
-  none: ({ classList }) => classList.add(classes.none),
-  show: ({ classList }) => classList.remove(classes.none)
+  none: 'ea_utilities__display-none'
 })
--- src/styles/visibility.js
+++ src/styles/visibility.js
@@ -1,10 +1,5 @@
 import object from '@mailobj-js/front/utils/object.js'
 
-export const classes = object(null, {
-  hidden: 'ea_utilities__visibility-hidden'
-})
-
 export default object(null, {
-  hidden: ({ classList }) => classList.add(classes.hidden),
-  unset: ({ classList }) => classList.remove(classes.hidden)
+  hidden: 'ea_utilities__visibility-hidden'
 })
