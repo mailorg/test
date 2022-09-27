13403cbceb268a720edf0f0bbada2b853e7d66e7
Michael Rouges <m.rouges@mailo.com>
2022-09-27 10:38:00 +0200

    + menu.open() & menu.close()

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 export * as debug from './debug/debug.js'
-export * as editors from '../editors/editors.js'
-/* @todo AJAX
-export * as fetchers from './fetchers/fetchers.js'
+export * as editors from './editors/editors.js'
+export * as menu from './menu/menu.js'
+/* @todo HISTORY
 export * as switchers from './switchers/switchers.js'
 */
---
+++ src/js/menu/menu.js
@@ -0,0 +1,76 @@
+import remove from '@mailobj-browser/front/js/tree/remove.js'
+import invoke from '@mailobj-browser/front/js/tree/invoke.js'
+import append from '@mailobj-browser/front/js/tree/append.js'
+import element from '@mailobj-browser/front/js/tree/element.js'
+import manager from '@mailobj-browser/front/js/contracts/manager.js'
+import object from '@mailobj-browser/front/js/utils/object.js'
+import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
+import click from '@mailobj-browser/front/js/events/types/click.js'
+import once from '@mailobj-browser/front/js/events/options/once.js'
+import passive from '@mailobj-browser/front/js/events/options/passive.js'
+import text from '@mailobj-browser/front/js/fetchers/text.js'
+
+let current = null
+
+const onClickOut = object(listener, {
+  type: click,
+  once,
+  passive,
+  task (
+    document,
+    { target }
+  ) {
+    if (current !== target && !current.contains(target)) {
+      close()
+    }
+  }
+})
+
+const render = async (
+  template,
+  url
+) => {
+  const { ownerDocument } = template
+  const { defaultView } = ownerDocument
+  const { Request } = defaultView
+  
+  if (url) {
+    const { fetched } = await text(object(null, {
+      request: new Request(`${url}`, {
+        credentials: true
+      })
+    }))
+    
+    template.innerHTML = fetched
+  }
+  
+  return invoke(template)
+}
+
+export const open = async (
+  template,
+  container,
+  { clientX, clientY },
+  url = template.dataset.url
+) => {
+  const { ownerDocument } = template
+  const body = element(ownerDocument, 'body')
+  const { children: [menu] } = render(template, url)
+  const { dataset } = menu
+  
+  close()
+  current = menu
+  Object.assign(dataset, { clientX, clientY })
+  append(body, menu)
+  onClickOut.listen(ownerDocument)
+  await manager.trigger(body)
+  append(container, menu)
+}
+
+export const close = () => {
+  if (current) {
+    remove(current)
+    onClickOut.forget(current)
+    current = null
+  }
+}
