07da6a5d7891e4f95f9b24318825e3c606d7d2b6
Mathieu Lessart <mlessart@mailobject.com>
2022-10-12 16:54:39 +0200

    RTE send header

--- src/js/rte/bkgs/insert.js
+++ src/js/rte/bkgs/insert.js
@@ -3,7 +3,7 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
-import {globals} from '../../ea/globals.js'
+import {globals} from '../../ea/ea.js'
 import {close, opener} from '../../menu/menu.js'
 import all from '@mailobj-browser/front/js/selectors/all.js'
 
