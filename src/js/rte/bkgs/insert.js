113de99b5e3bf25d63540e254b094672a3b3b674
Mathieu Lessart <mlessart@mailobject.com>
2022-10-12 16:54:00 +0200

    RTE send header

--- src/js/rte/bkgs/insert.js
+++ src/js/rte/bkgs/insert.js
@@ -3,8 +3,9 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
-import globals from '../../ea/globals.js'
+import {globals} from '../../ea/globals.js'
 import {close, opener} from '../../menu/menu.js'
+import all from '@mailobj-browser/front/js/selectors/all.js'
 
 const onClick = object(listener, {
   type: click,
