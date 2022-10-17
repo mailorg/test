8c046805cc0032d00665f0093a7a4855fb78f4ff
Michael Rouges <m.rouges@mailo.com>
2022-10-17 14:46:26 +0200

    debug

--- src/js/fixed/fixed.js
+++ src/js/fixed/fixed.js
@@ -127,7 +127,7 @@ export const fromNode = (
       
       if (bottom + height > clientHeight) {
         clientY = top - height
-        console.log({ clientY, top, height })
+        console.log(rect(target), rect(node))
       } else {
         clientY = bottom
       }
