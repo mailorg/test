2715dcea49d96e2ed1dc12b8484b812d7853fc82
Valentin <valentin.dehouck@mailo.com>
2022-10-25 14:07:44 +0200

    Add specifics hotkeys for mailbox

---
+++ src/js/hotkey/remove.js
@@ -0,0 +1,18 @@
+import object from '@mailobj-browser/front/js/utils/object.js'
+import one from '@mailobj-browser/front/js/selectors/one.js'
+import globals from '@mailobj-browser/utilities/js/ea/globals.js'
+
+export default object(null, {
+    task(
+        element
+    ) {
+        const {ownerDocument, dataset} = element
+        const {body} = ownerDocument
+        const {confirm} = dataset
+        const {selector, name, action} = this
+        const e = one(selector, body)
+        e.value = action
+        globals.ea_list2(e, "", name, null, confirm, null)
+    }
+})
+
