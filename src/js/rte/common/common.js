c83d18a4738fb1951a77db464cf5c05ebaa5cf23
Mathieu Lessart <mlessart@mailobject.com>
2022-11-17 15:55:01 +0100

    rte contracts

---
+++ src/js/rte/common/common.js
@@ -0,0 +1,9 @@
+import contract from '@mailobj-browser/front/js/contracts/contract.js'
+import array from '@mailobj-browser/front/js/utils/array.js'
+import hr from './hr.js'
+
+export default contract ({
+  hr: array([
+    hr
+  ]),
+})
