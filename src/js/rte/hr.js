68ddc8c6846312322892578c965ada346f56477d
Mathieu Lessart <mlessart@mailobject.com>
2022-11-17 15:23:57 +0100

    rte contracts

--- src/js/rte/hr.js
+++ src/js/rte/hr.js
@@ -2,7 +2,7 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
-import globals from '../../ea/globals.js'
+import globals from '../ea/globals.js'
 import rte from '../rte.js'
 
 const onClick = object(listener, {
