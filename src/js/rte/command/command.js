f07615732123961fa45ed1bbdc493e9b5dd3bda1
Mathieu Lessart <mlessart@mailobject.com>
2022-11-18 00:52:39 +0100

    rte contracts

--- src/js/rte/command/command.js
+++ src/js/rte/command/command.js
@@ -1,6 +1,7 @@
 import contract from '@mailobj-browser/front/js/contracts/contract.js'
 import array from '@mailobj-browser/front/js/utils/array.js'
 import clear from './clear.js'
+import formatBlock from './formatBlock.js'
 import hr from './hr.js'
 import ol from './ol.js'
 import redo from './redo.js'
@@ -11,6 +12,9 @@ import removeFormat from './removeFormat.js'
 export default contract ({
   clear: array([
     clear
+  ]),
+  formatBlock: array([
+    formatBlock
   ]),
   hr: array([
     hr
