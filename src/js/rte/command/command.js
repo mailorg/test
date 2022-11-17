60d9b7486a49d35d282c06bc4b606be7f47c4058
Mathieu Lessart <mlessart@mailobject.com>
2022-11-17 23:00:04 +0100

    rte contracts

--- src/js/rte/command/command.js
+++ src/js/rte/command/command.js
@@ -2,6 +2,9 @@ import contract from '@mailobj-browser/front/js/contracts/contract.js'
 import array from '@mailobj-browser/front/js/utils/array.js'
 import clear from './clear.js'
 import hr from './hr.js'
+import redo from './redo.js'
+import undo from './undo.js'
+import removeFormat from './removeFormat.js'
 
 export default contract ({
   clear: array([
@@ -9,5 +12,14 @@ export default contract ({
   ]),
   hr: array([
     hr
+  ]),
+  redo: array([
+    redo
+  ]),
+  removeFormat: array([
+    removeFormat
+  ]),
+  undo: array([
+    undo
   ]),
 })
---
+++ src/js/rte/command/redo.js
@@ -0,0 +1,25 @@
+import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
+import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
+import click from '@mailobj-browser/front/js/events/types/click.js'
+import object from '@mailobj-browser/front/js/utils/object.js'
+import globals from '../../ea/globals.js'
+import rte from '../rte.js'
+
+const onClick = object(listener, {
+  type: click,
+  hooks: [preventDefault],
+  task () {
+    globals.ea_rte_command('id_msg_text', 'redo', '')
+  }
+})
+
+export const test = (
+  text
+) => {
+  return text
+}
+
+export default button => {
+  onClick.listen(button)
+  rte(button, object(null, { test }))
+}
