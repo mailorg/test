547b4c724a59169fe6ceed17818f47cd26ac6d49
Michael Rouges <m.rouges@mailo.com>
2022-11-25 11:21:20 +0100

    + wait.removed(node, abort = never)

---
+++ src/js/lightboxes/openers/button.js
@@ -0,0 +1,33 @@
+import one from '@mailobj-browser/front/js/selectors/one.js'
+import object from '@mailobj-browser/front/js/utils/object.js'
+import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
+import click from '@mailobj-browser/front/js/events/types/click.js'
+import passive from '@mailobj-browser/front/js/events/options/passive.js'
+import { parse } from '@mailobj-browser/utilities/js/lightboxes/lightbox.js'
+
+const openers = new WeakMap()
+
+const onClick = object(listener, {
+  type: click,
+  passive,
+  async task (
+    button
+  ) {
+    const template = one(':scope > template', button)
+    const lightbox = await parse(template)
+    
+    openers.set(lightbox, button)
+  }
+})
+
+export const listen = (
+  button
+) => {
+  onClick.listen(button)
+}
+
+export const opener = (
+  lightbox
+) => {
+  return openers.get(lightbox)
+}
---
+++ src/js/wait/removed.js
@@ -0,0 +1,40 @@
+import resolvable from '@mailobj-browser/front/js/utils/resolvable.js'
+import never from '@mailobj-browser/front/js/utils/never.js'
+
+const observers = new WeakMap()
+const resolvers = new WeakMap()
+
+const options = {
+  childList: true
+}
+
+const callback = records => {
+  for (const { removedNodes } of records) {
+    for (const node of removedNodes) {
+      resolvers.get(node)?.(node)
+    }
+  }
+}
+
+const observing = ownerDocument => {
+  if (!observers.has(ownerDocument)) {
+    const { defaultView } = ownerDocument
+    const { MutationObserver } = defaultView
+    const observer = new MutationObserver(callback)
+    
+    observers.set(ownerDocument, observer)
+  }
+  
+  return observers.get(ownerDocument)
+}
+
+export default (node, abort = never) => {
+  const { ownerDocument, parentNode } = node
+  const [promise, { resolve }] = resolvable()
+  const observer = observing(ownerDocument)
+  
+  observer.observe(parentNode, options)
+  resolvers.set(node, resolve)
+  
+  return Promise.race([abort, promise])
+}
