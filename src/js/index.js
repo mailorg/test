d20b4482c763b1b98d2908ba511dba5ac530592e
francois <fbouhours@mailobject.com>
2022-12-06 10:43:22 +0100

    Send pages

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ export * as debug from './debug/debug.js'
 export * as editors from './editors/editors.js'
 export * as fixed from './fixed/fixed.js'
 export * as menu from './menu/menu.js'
+export * as touchGestures from './touchGestures/touchGestures.js'
 /* @todo HISTORY
 export * as switchers from './switchers/switchers.js'
 */
