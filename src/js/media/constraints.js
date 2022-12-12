0e2eb7fd4b73d10b42ae74f4b9e8fab8fd4d4cdf
git <ea@dev-1.localhost>
2022-12-12 15:44:45 +0100

    JM use media constraints

---
+++ src/js/media/constraints.js
@@ -0,0 +1,14 @@
+import object from '@mailobj-browser/front/js/utils/object.js'
+
+export const audioAndVideo = object(null, {
+  audio: 1,
+  video: 1
+})
+
+export const audioOnly = object(null, {
+  audio: 1
+})
+
+export const videoOnly = object(null, {
+  video: 1
+})
--- src/js/media/stream.js
+++ src/js/media/stream.js
@@ -1,3 +1,9 @@
-export default (constraints) => {
+export const get = (constraints) => {
   return navigator.mediaDevices.getUserMedia(constraints)
+}
+
+export const stop = (stream) => {
+  stream.getTracks().forEach((track) => {
+    track.stop()
+  })
 }
