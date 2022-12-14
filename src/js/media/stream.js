0f2d6098799809e6ccb4a1005ede3dc143b4b4b1
Michael Rouges <m.rouges@mailo.com>
2022-12-14 15:56:57 +0100

    + media.stream.has()

--- src/js/media/stream.js
+++ src/js/media/stream.js
@@ -1,9 +1,41 @@
-export const get = (constraints) => {
-  return navigator.mediaDevices.getUserMedia(constraints)
+import object from '@mailobj-browser/front/js/utils/object.js'
+import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
+import passive from '@mailobj-browser/front/js/events/options/passive.js'
+
+const tracks = object(null, {
+  audio: new Set(),
+  video: new Set()
+})
+
+const onEnded = object(listener, {
+  type: 'ended',
+  passive,
+  task (track) {
+    const { kind } = track
+    
+    tracks[kind].delete(track)
+  }
+})
+
+export const get = async (window, constraints) => {
+  const { navigator } = constraints
+  const { mediaDevices } = navigator
+  const media = await mediaDevices.getUserMedia(constraints)
+  
+  for (const track of media.getTracks()) {
+    const { kind } = track
+    
+    onEnded.listen(track)
+    tracks[kind].add(track)
+  }
 }
 
-export const stop = (stream) => {
-  stream.getTracks().forEach((track) => {
+export const has = kind => {
+  return tracks[kind].size > 0
+}
+
+export const stop = stream => {
+  for (const track of stream.getTracks()) {
     track.stop()
-  })
+  }
 }
