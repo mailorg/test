a139ac64ae6446681d501fa3e43243b123cc9614
francois <fbouhours@mailobject.com>
2022-12-20 17:52:21 +0100

    Send page

---
+++ src/js/rte/advanced_actions/advanced_actions.js
@@ -0,0 +1,9 @@
+import contract from '@mailobj-browser/front/js/contracts/contract.js'
+import array from '@mailobj-browser/front/js/utils/array.js'
+import advanced_actions from './show_more'
+
+export default contract({
+  advanced_actions: array([
+    advanced_actions
+  ]),
+})
---
+++ src/js/rte/advanced_actions/show_more.js
@@ -0,0 +1,20 @@
+import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
+import click from '@mailobj-browser/front/js/events/types/click.js'
+import object from '@mailobj-browser/front/js/utils/object.js'
+// import one from '@mailobj-browser/front/js/selectors/one.js'
+// import { elements, modifiers } from '../../../styles.js'
+import { modifiers as genericsModifiers } from '@mailobj-browser/components-generics/js/styles.js'
+
+const onClick = object(listener, {
+  type: click,
+  task(
+    button
+  ) {
+    console.log(button)
+    // button.classList.toggle(genericsModifiers.show_more.hide)
+  }
+})
+
+export default button => {
+  onClick.listen(button)
+}
