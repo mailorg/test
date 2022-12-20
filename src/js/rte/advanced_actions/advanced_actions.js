f072b258683f822d917b795d6db22630bbe717ca
francois <fbouhours@mailobject.com>
2022-12-20 17:57:19 +0100

    Send page

--- src/js/rte/advanced_actions/advanced_actions.js
+++
@@ -1,9 +0,0 @@
-import contract from '@mailobj-browser/front/js/contracts/contract.js'
-import array from '@mailobj-browser/front/js/utils/array.js'
-import advanced_actions from './show_more'
-
-export default contract({
-  advanced_actions: array([
-    advanced_actions
-  ]),
-})
