df240b9a0f8b13461ab9d5ccc8e8ba58269eef86
francois <fbouhours@mailobject.com>
2022-12-21 11:04:20 +0100

    Send page

--- src/js/rte/advanced_actions.js
+++ src/js/rte/advanced_actions.js
@@ -2,7 +2,7 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
-import { elements, modifiers } from '../../../styles.js'
+// import { elements, modifiers } from '../../../styles.js'
 import { modifiers as genericsModifiers } from '@mailobj-browser/components-generics/js/styles.js'
 
 const onClick = object(listener, {
