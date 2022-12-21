327727e22cd5972dd623dbed31e240e2912f5450
francois <fbouhours@mailobject.com>
2022-12-21 10:55:39 +0100

    Send page

--- src/js/rte/advanced_actions.js
+++ src/js/rte/advanced_actions.js
@@ -1,8 +1,8 @@
 import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
-// import one from '@mailobj-browser/front/js/selectors/one.js'
-// import { elements, modifiers } from '../../../styles.js'
+import one from '@mailobj-browser/front/js/selectors/one.js'
+import { elements, modifiers } from '../../../styles.js'
 import { modifiers as genericsModifiers } from '@mailobj-browser/components-generics/js/styles.js'
 
 const onClick = object(listener, {
@@ -10,8 +10,10 @@ const onClick = object(listener, {
   task(
     button
   ) {
-    console.log(button)
     button.classList.toggle(genericsModifiers.show_more.hide)
+    const parent = button.closest('div')
+    const containerActions = one('.ea_generics__rte_advanced_actions', parent)
+    containerActions.classList.toggle(modifiers.display.none)
   }
 })
 
