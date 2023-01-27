fa00e4d807f3cb9f6e1d1f5ebb840d6f24c296f2
Michael Rouges <m.rouges@mailo.com>
2023-01-27 17:53:10 +0100

    debug

--- src/js/lightboxes/menus/menu.js
+++ src/js/lightboxes/menus/menu.js
@@ -7,10 +7,17 @@ import one from '@mailobj-browser/front/js/selectors/one.js'
 import keyDown from '@mailobj-browser/front/js/events/types/keyDown.js'
 import scroll from '@mailobj-browser/front/js/events/types/scroll.js'
 import { fromEvent, fromNode, move, resize } from '../../fixed/fixed.js'
-import { autoClose, close, parse } from '../lightbox.js'
+import * as lightbox from '../lightbox.js'
 import { container, template } from '../openers/template.js'
+import blur from '@mailobj-browser/front/js/events/types/blur.js'
+import keyUp from '@mailobj-browser/front/js/events/types/keyUp.js'
 
-export { autoClose, close }
+let current = null
+
+export const close = () => {
+  current = null
+  lightbox.close()
+}
 
 const openers = new WeakMap()
 
@@ -19,6 +26,38 @@ const onScroll = object(listener, {
   once,
   passive,
   task: close
+})
+
+const onBlur = object(listener, {
+  type: blur,
+  capture,
+  passive,
+  task (
+    document
+  ) {
+    const { defaultView } = document
+    const { requestAnimationFrame } = defaultView
+    
+    requestAnimationFrame(() => {
+      const { activeElement } = document
+      
+      if (current && activeElement && current !== activeElement && !current.contains(activeElement)) {
+        close()
+      }
+    })
+  }
+})
+
+const onEscape = object(listener, {
+  type: keyUp,
+  task: (document, event) => {
+    const { key } = event
+    
+    if (current && key === 'Escape') {
+      preventDefault(event)
+      close()
+    }
+  }
 })
 
 export const onKeyDown = object(listener, {
@@ -45,8 +84,8 @@ export const open = async (
   event = null
 ) => {
   const { ownerDocument } = opener
-  const content = await parse(template(opener), container(opener))
-
+  const content = await lightbox.parse(template(opener), container(opener))
+  
   close()
   move(content)
   
@@ -60,7 +99,9 @@ export const open = async (
   
   focus(content)
   onScroll.listen(ownerDocument)
-  autoClose(content)
+  onBlur.listen(ownerDocument)
+  onEscape.listen(ownerDocument)
+  current = content
   
   return content
 }
