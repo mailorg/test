68424fb7ba5c278eacbcbe0297cb98757ebf5c34
Mathieu Lessart <mlessart@mailobject.com>
2023-02-03 14:24:20 +0100

    utilities.rte.specials

--- src/js/rte/specials/insert.js
+++ src/js/rte/specials/insert.js
@@ -1,7 +1,7 @@
 import click from '@mailobj-browser/front/js/events/types/click.js'
 import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
-import * as lightbox from '../lightbox.js'
+import * as lightbox from '../../lightboxes/lightbox.js'
 import {close} from '../../lightboxes/menus/menu.js'
 import {globals} from '../../ea/ea.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
