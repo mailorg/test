1a8c82f7ad4a76259607932a0513c80c54e27877
Mathieu Lessart <mlessart@mailobject.com>
2023-02-03 15:22:08 +0100

    utilities.rte.smileys

--- src/js/rte/smileys/smileys.js
+++ src/js/rte/smileys/smileys.js
@@ -1,10 +1,10 @@
 import contract from '@mailobj-browser/front/js/contracts/contract.js'
 import array from '@mailobj-browser/front/js/utils/array.js'
-import insert from '@mailobj-browser/utilities/js/rte/smileys/insert.js'
+import insertSmiley from './insert.js'
 
 export default contract({
   insert: array([
-    insert
+    insertSmiley
   ])
 })
 
