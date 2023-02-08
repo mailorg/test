f689b742ef2d19c968e4ff21b1220093c5c663fa
francois <fbouhours@mailobject.com>
2023-02-08 17:59:29 +0100

    context menu library

--- src/js/lightboxes/menus/paginated.js
+++ src/js/lightboxes/menus/paginated.js
@@ -86,13 +86,15 @@ export default async (
   const rows = list.getAttribute('data-rows')
   console.log(rows);
   const columns = list.getAttribute('data-columns')
+  const columnHeight = list.getAttribute('data-column-height')
+  const columnMaxWidth = list.getAttribute('data-column-max-width')
   const columnWidth = list.getAttribute('data-column-width')
-  const columnSize = list.getAttribute('data-column-size')
   const {style} = list
   style.setProperty('--ea_context_menu_library_rows', rows)
   style.setProperty('--ea_context_menu_library_columns', columns)
+  style.setProperty('--ea_context_menu_library_columns_height', columnHeight)
+  style.setProperty('--ea_context_menu_library_columns_max_width', columnMaxWidth)
   style.setProperty('--ea_context_menu_library_columns_width', columnWidth)
-  style.setProperty('--ea_context_menu_library_columns_size', columnSize)
 
   paginations.set(list, { children, items, page, pages, size })
   onKeyDown.listen(list)
