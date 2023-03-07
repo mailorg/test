34a9adeb076782e7bb24fb1c3a35bc2f9d5b8763
francois <fbouhours@mailobject.com>
2023-03-07 16:02:35 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,27 +1,21 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
 const screenSizes = object(null, {
-    xs: 576,
-    sm: 768,
-    md: 992,
-    lg: 1200,
+  xxs: 0,
+  xs: 360,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200
 })
 
 export const device = object(null, {
-    isScreenXs: () => {
-        const width = window.innerWidth
-        return width <= screenSizes.xs
-    },
-    isScreenSm: () => {
-        const width = window.innerWidth
-        return !!(width >= screenSizes.sm && width < screenSizes.md)
-    },
-    isScreenMd: () => {
-        const width = window.innerWidth
-        return !!(width >= screenSizes.md && width < screenSizes.lg)
-    },
-    isScreenLg: () => {
-        const width = window.innerWidth
-        return width >= screenSizes.lg
-    },
+  isLowerThanMd: () => {
+    const width = window.innerWidth
+    return width <= screenSizes.md
+  },
+  isGreaterThanMd: () => {
+    const width = window.innerWidth
+    return width > screenSizes.md
+  },
 })
