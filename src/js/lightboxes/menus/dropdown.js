a2756b3276d4ee018270a481c89ec659d006078a
Michael Rouges <m.rouges@mailo.com>
2023-03-10 16:01:21 +0100

    debug dropdown focus

--- src/js/lightboxes/menus/dropdown.js
+++ src/js/lightboxes/menus/dropdown.js
@@ -1,6 +1,7 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 import * as menu from './menu.js'
 import { display } from './menu.js'
+import all from '@mailobj-browser/front/js/selectors/all.js'
 
 export const {
   close,
@@ -22,9 +23,20 @@ const onKeyDown = object(menu.onKeyDown, {
   keys
 })
 
-export default (
+export default async (
   list
 ) => {
+  const select = opener(list)
+  const { value = '' } = select
+  
   onKeyDown.listen(list)
-  display(list, opener(list))
+  await display(list, select)
+  
+  for (const input of all('input', list)) {
+    if (input.value === value) {
+      input.click()
+      input.focus()
+      break
+    }
+  }
 }
