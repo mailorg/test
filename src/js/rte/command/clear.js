80f7f885bc03cc060d5b0ea320e65d7cb5765a66
francois <fbouhours@mailobject.com>
2023-03-10 14:44:05 +0100

    Send page

--- src/js/rte/command/clear.js
+++ src/js/rte/command/clear.js
@@ -1,31 +1,20 @@
 import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
-import touchStart from '@mailobj-browser/front/js/events/types/touchStart.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
-import rte, {command_clear} from '../rte.js'
+import rte from '../rte.js'
 
 const onClick = object(listener, {
   type: click,
   hooks: [preventDefault],
-  task() {
-    command_clear('id_msg_text')
-  }
-})
-
-const onTouchStart = object(listener, {
-  type: touchStart,
-  hooks: [preventDefault],
   task(button) {
-    // command_clear('id_msg_text')
     const {contentDocument} = one('#id_msg_text', button.closest('form'))
     const {body} = contentDocument
-    const { dataset } = button
+    const {dataset} = button
     const msg = dataset.confirm
 
     if (confirm(msg)) {
-      console.log(body);
       body.innerHTML = ""
     }
   }
@@ -38,7 +27,6 @@ export const test = (
 }
 
 export default button => {
-  // onClick.listen(button)
-  onTouchStart.listen(button)
+  onClick.listen(button)
   rte(button, object(null, {test}))
 }
