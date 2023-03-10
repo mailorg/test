3767e76fc63b271a8bd0db276436986b732b6b39
francois <fbouhours@mailobject.com>
2023-03-10 11:04:16 +0100

    Send page

--- src/js/rte/advanced_actions.js
+++ src/js/rte/advanced_actions.js
@@ -1,12 +1,15 @@
 import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
+import touchStart from '@mailobj-browser/front/js/events/types/touchStart.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
+import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
 import { elements } from './styles.js'
 import { modifiers as genericsModifiers } from '@mailobj-browser/components-generics/js/styles.js'
 
 const onClick = object(listener, {
   type: click,
+  hooks: [preventDefault],
   task(
     button
   ) {
@@ -17,6 +20,17 @@ const onClick = object(listener, {
   }
 })
 
+const onTouchStart = object(listener, {
+  type: touchStart,
+  hooks: [preventDefault],
+  task(
+    button
+  ) {
+    onClick.task(button)
+  }
+})
+
 export default button => {
   onClick.listen(button)
+  onTouchStart.listen(button)
 }
