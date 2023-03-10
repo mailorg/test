9b9ce654c2000be4e2d7e9c2ee8d026d8b29f4c4
francois <fbouhours@mailobject.com>
2023-03-10 11:34:30 +0100

    Send page

--- src/js/rte/command/clear.js
+++ src/js/rte/command/clear.js
@@ -2,22 +2,19 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
 import touchStart from '@mailobj-browser/front/js/events/types/touchStart.js'
+import one from '@mailobj-browser/front/js/selectors/one.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import rte, {command_clear} from '../rte.js'
 
 const onClick = object(listener, {
   type: click,
   hooks: [preventDefault],
-  task() {
-    command_clear('id_msg_text')
-  }
-})
-
-const onTouchStart = object(listener, {
-  type: touchStart,
-  hooks: [preventDefault],
   task(button) {
-    onClick.task(button)
+    const id = 'id_msg_text'
+    const {ownerDocument} = button
+    const iframe = one(id, ownerDocument)
+    command_clear(id)
+    iframe.focus()
   }
 })
 
@@ -29,6 +26,5 @@ export const test = (
 
 export default button => {
   onClick.listen(button)
-  onTouchStart.listen(button)
   rte(button, object(null, {test}))
 }
