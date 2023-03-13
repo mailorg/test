2a85372380f000c4ef84699e959e974012ccad48
francois <fbouhours@mailobject.com>
2023-03-13 12:51:01 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -8,7 +8,7 @@ const screenSizes = object(null, {
   xxs: 0,
   xs: 360,
   sm: 576,
-  md: style.getProperty('--ea_breakpoint_md'),
+  md: style.getPropertyValue('--ea_breakpoint_md'),
   lg: 992,
   xl: 1200
 })
