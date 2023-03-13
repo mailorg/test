c531f69dcbde0afe3df3b7496f4630cd2421bd4c
francois <fbouhours@mailobject.com>
2023-03-13 14:51:35 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,7 +1,7 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
 const {documentElement} = document
-console.log(documentElement);
+console.log(getComputedStyle(documentElement));
 const {style} = documentElement
 const screenSizes = object(null, {
   xxs: style.getPropertyValue('--ea_breakpoint_xxs'),
