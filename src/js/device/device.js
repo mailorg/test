6152e1945d2cf082fdc3bf806a4bcb2f6225915a
francois <fbouhours@mailobject.com>
2023-03-13 12:34:07 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,8 +1,8 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
 const {activeElement} = document
-const {style} = activeElement
-console.log(activeElement)
+const {style} = document
+console.log(style)
 const screenSizes = object(null, {
   xxs: 0,
   xs: 360,
