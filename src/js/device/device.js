42a9f668f8105bf3e388765aea5b450661692d65
francois <fbouhours@mailobject.com>
2023-03-13 15:00:28 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,7 +1,6 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
 const {documentElement} = document
-console.log(getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_md'));
 const screenSizes = object(null, {
   xxs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xxs').slice(0, -3),
   xs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xs').slice(0, -3),
@@ -13,7 +12,6 @@ const screenSizes = object(null, {
 
 export const device = object(null, {
   isLowerThanMd: () => {
-    console.log(screenSizes)
     const width = window.innerWidth
     return width <= screenSizes.md
   },
