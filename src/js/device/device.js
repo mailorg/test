e8b511d4f925513966116705e40a2647ef1e56d8
francois <fbouhours@mailobject.com>
2023-03-13 14:52:14 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,15 +1,15 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
 const {documentElement} = document
-console.log(getComputedStyle(documentElement));
+console.log();
 const {style} = documentElement
 const screenSizes = object(null, {
-  xxs: style.getPropertyValue('--ea_breakpoint_xxs'),
-  xs: style.getPropertyValue('--ea_breakpoint_xs'),
-  sm: style.getPropertyValue('--ea_breakpoint_sm'),
-  md: style.getPropertyValue('--ea_breakpoint_md'),
-  lg: style.getPropertyValue('--ea_breakpoint_lg'),
-  xl: style.getPropertyValue('--ea_breakpoint_xl')
+  xxs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xxs'),
+  xs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xs'),
+  sm: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_sm'),
+  md: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_md'),
+  lg: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_lg'),
+  xl: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xl')
 })
 
 export const device = object(null, {
