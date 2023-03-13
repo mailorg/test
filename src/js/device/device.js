a23fd641a5217040978e4e22efdab9feb3d8a9b9
francois <fbouhours@mailobject.com>
2023-03-13 12:08:20 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,6 +1,6 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 import { md } from '@mailobj-browser/components-generics/scss/abstracts/_variables.scss'
-
+console.log(md ,"coucou")
 export const device = object(null, {
   isLowerThanMd: () => {
     console.log(md)
