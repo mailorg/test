0783fea1ef41144ce7133797ef55c8d1fcb14bbb
francois <fbouhours@mailobject.com>
2023-03-13 14:57:39 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -3,17 +3,17 @@ import object from '@mailobj-browser/front/js/utils/object.js'
 const {documentElement} = document
 console.log(getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_md'));
 const screenSizes = object(null, {
-  xxs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xxs'),
-  xs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xs'),
-  sm: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_sm'),
-  md: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_md'),
-  lg: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_lg'),
-  xl: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xl')
+  xxs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xxs').slice(0, -2),
+  xs: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xs').slice(0, -2),
+  sm: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_sm').slice(0, -2),
+  md: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_md').slice(0, -2),
+  lg: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_lg').slice(0, -2),
+  xl: getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint_xl').slice(0, -2)
 })
 
 export const device = object(null, {
   isLowerThanMd: () => {
-    console.log(screenSizes.md)
+    console.log(screenSizes)
     const width = window.innerWidth
     return width <= screenSizes.md
   },
