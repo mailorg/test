3ff4007b6c82c8b6180fe62af028689c52431b08
francois <fbouhours@mailobject.com>
2023-03-13 12:13:31 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,14 +1,23 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
-import { md } from '@mailobj-browser/components-generics/scss/abstracts/_variables.scss'
-console.log(md ,"coucou")
+// import { md } from '@mailobj-browser/components-generics/scss/abstracts/_variables.scss'
+
+const screenSizes = object(null, {
+  xxs: 0,
+  xs: 360,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200
+})
+
 export const device = object(null, {
   isLowerThanMd: () => {
-    console.log(md)
+    console.log(screenSizes.md)
     const width = window.innerWidth
-    return width <= md
+    return width <= screenSizes.md
   },
   isGreaterThanMd: () => {
     const width = window.innerWidth
-    return width > md
+    return width > screenSizes.md
   },
 })
