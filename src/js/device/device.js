a475215d3b42d2c2cdfbe486ad37348ff7e39a65
francois <fbouhours@mailobject.com>
2023-03-15 16:53:43 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -9,14 +9,14 @@ const screenSizes = object(null, {
   xl: 6
 })
 
-export const isLowerThan = async (node, key) => {
+export const isLowerThan = (node, key) => {
   const {ownerDocument = node} = node
   const {documentElement, defaultView} = ownerDocument
   const {getComputedStyle} = defaultView
   return screenSizes[key] > screenSizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
 }
 
-export const isGreaterThan = async (node, key) => {
+export const isGreaterThan = (node, key) => {
   const {ownerDocument = node} = node
   const {documentElement, defaultView} = ownerDocument
   const {getComputedStyle} = defaultView
