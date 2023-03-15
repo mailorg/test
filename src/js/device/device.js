13c57b98179440f7c32289a7f923089de0dc13e4
francois <fbouhours@mailobject.com>
2023-03-15 16:37:49 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -14,7 +14,8 @@ export const isLowerThan = (element, key) => {
   const {documentElement} = ownerDocument
   console.log(screenSizes[key]);
   // console.log(screenSizes[xxs]);
-  console.log(screenSizes['xxs']);
+  const breakpoint = getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')
+  console.log(screenSizes[breakpoint]);
   return screenSizes[key] > screenSizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
 }
 
