615749a44bf372be2df5dd71aaf516fa6d06a9a0
francois <fbouhours@mailobject.com>
2023-03-15 17:14:57 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -10,17 +10,17 @@ export const sizes = object(null, {
 })
 
 export const isLowerThan = async (node, key) => {
+  const map = new Map(Object.entries(sizes).map(([key, value]) => [value, key]))
   const {ownerDocument = node} = node
   const {documentElement, defaultView} = ownerDocument
   const {getComputedStyle} = defaultView
-  const map = new Map(Object.entries(sizes).map(([key, value]) => [value, key]))
-  console.log(map);
-  return key > sizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
+  return map.get(key) > sizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
 }
 
 export const isGreaterThan = async (node, key) => {
+  const map = new Map(Object.entries(sizes).map(([key, value]) => [value, key]))
   const {ownerDocument = node} = node
   const {documentElement, defaultView} = ownerDocument
   const {getComputedStyle} = defaultView
-  return key < sizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
+  return map.get(key) < sizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
 }
