7cdfd3a37528388a2074fe973144e721ed4423b8
francois <fbouhours@mailobject.com>
2023-03-15 17:04:20 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -13,6 +13,8 @@ export const isLowerThan = async (node, key) => {
   const {ownerDocument = node} = node
   const {documentElement, defaultView} = ownerDocument
   const {getComputedStyle} = defaultView
+  const map = new Map(Object.entries(sizes).map(([key, value]) => [value, key]))
+  console.log(map);
   return key > sizes[getComputedStyle(documentElement).getPropertyValue('--ea_breakpoint')]
 }
 
