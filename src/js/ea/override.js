3541f3f942737f2c8b25e249712adcc59530488d
Michael Rouges <m.rouges@mailo.com>
2023-03-15 15:11:10 +0100

    turns override as function

--- src/js/ea/override.js
+++ src/js/ea/override.js
@@ -1,34 +1,37 @@
-import { disable } from '@mailobj-browser/components-generics/js/hotkey/hotkey.js'
 import resolvable from '@mailobj-browser/front/js/utils/resolvable.js'
 
-const { ea_context_menu_, ea_context_menu_close } = globalThis
-
-let resolver = resolvable()
-
-const next = () => {
-    resolve()
-    resolver = resolvable()
-
-    return resolver[0]
-}
-
-const resolve = () => {
-    const [, { resolve }] = resolver
-
-    resolve()
-}
-
-globalThis.ea_context_menu_ = (element, event, ...params) => {
-    const node = element ?? event.target
+export default disable => {
+  try {
+    const { ea_context_menu_, ea_context_menu_close } = globalThis
     
-    if (node) {
-        disable(node, next())
+    let resolver = resolvable()
     
-        return ea_context_menu_(element, event, ...params)
+    const next = () => {
+      resolve()
+      resolver = resolvable()
+      
+      return resolver[0]
     }
-}
-
-globalThis.ea_context_menu_close = () => {
-    resolve()
-    return ea_context_menu_close()
+    
+    const resolve = () => {
+      const [, { resolve }] = resolver
+      
+      resolve()
+    }
+    
+    globalThis.ea_context_menu_ = (element, event, ...params) => {
+      const node = element ?? event.target
+      
+      if (node) {
+        disable(node, next())
+        
+        return ea_context_menu_(element, event, ...params)
+      }
+    }
+    
+    globalThis.ea_context_menu_close = () => {
+      resolve()
+      return ea_context_menu_close()
+    }
+  } catch {}
 }
