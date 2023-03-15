d4da67af529e4b2b1765089c24ad26f76331445c
francois <fbouhours@mailobject.com>
2023-03-15 16:20:17 +0100

    Send page

--- src/js/device/device.js
+++ src/js/device/device.js
@@ -1,11 +1,20 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
+const screenSizes = object(null, {
+  xxs: 1,
+  xs: 2,
+  sm: 3,
+  md: 4,
+  lg: 5,
+  xl: 6
+})
+
 export const isLowerThan = (element, key) => {
   const {ownerDocument} = element
-  return key === getComputedStyle(ownerDocument).getPropertyValue('--ea_breakpoint')
+  return screenSizes[key] <= screenSizes[getComputedStyle(ownerDocument).getPropertyValue('--ea_breakpoint')]
 }
 
 export const isGreaterThan = (element, key) => {
   const {ownerDocument} = element
-  return key === getComputedStyle(ownerDocument).getPropertyValue('--ea_breakpoint')
+  return screenSizes[key] >= screenSizes[getComputedStyle(ownerDocument).getPropertyValue('--ea_breakpoint')]
 }
