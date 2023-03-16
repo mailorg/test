c656998665d07425091dfd169918047829f74836
Michael Rouges <m.rouges@mailo.com>
2023-03-16 17:29:57 +0100

    fix styles names

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,7 +1,7 @@
 import object from '@mailobj-browser/front/js/utils/object.js'
 
-const block = 'ea_utilities__'
+const block = 'ea_utilities'
 
 export const elements = object(null, {
-  aside_lightboxes: `${block}_aside_lightboxes`,
+  aside_lightboxes: `${block}__aside_lightboxes`,
 })
