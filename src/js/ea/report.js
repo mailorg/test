d0fd68bdbe2aa125829726a8a03110824336c2cb
git <ea@dev-1.localhost>
2023-03-23 10:59:30 +0100

    JMD report() : manage String and Array

--- src/js/ea/report.js
+++ src/js/ea/report.js
@@ -2,8 +2,10 @@ import fetch from '@mailobj-browser/front/js/fetchers/fetch.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import href from './href.js'
 
-export default async (values) => {
+export default async (param) => {
 
+  /* param peut être string ou Array : */
+  const values = [param].flat()
   const url = href('util/log')
   const keys = Array.from({length: values.length}, (x, i) => 'param_' + i)
   const data = new FormData()
