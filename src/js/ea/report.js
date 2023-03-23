0a532fd24935fc3a248fba5b0dfe87293ca3ddc7
git <ea@dev-1.localhost>
2023-03-23 11:26:57 +0100

    JMD retrieve properly FormData and Request

--- src/js/ea/report.js
+++ src/js/ea/report.js
@@ -1,10 +1,13 @@
 import fetch from '@mailobj-browser/front/js/fetchers/fetch.js'
+import defaults from '@mailobj-browser/front/js/selectors/defaults.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import href from './href.js'
 
 export default async (param) => {
 
   console.error(param)
+  const { window } = defaults
+  const { FormData, Request } = window
   /* param peut être string ou Array : */
   const values = [param].flat()
   const url = href('util/log')
