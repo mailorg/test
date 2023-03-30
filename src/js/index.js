ba9f63042be900fa4143eaa99f05006dbcf2b98a
francois <fbouhours@mailobject.com>
2023-03-30 10:00:47 +0200

    Pages out

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 export * as debug from './debug/debug.js'
 export * as editors from './editors/editors.js'
 export * as fixed from './fixed/fixed.js'
-// export * as menu from './menu/menu.js'
 /* @todo HISTORY
 export * as switchers from './switchers/switchers.js'
 */
