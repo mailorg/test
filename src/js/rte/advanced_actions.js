e6be6cfd3d075f3222e0101b6f82a70b25872cb2
francois <fbouhours@mailobject.com>
2023-04-14 12:13:00 +0200

    import javascript styles

--- src/js/rte/advanced_actions.js
+++ src/js/rte/advanced_actions.js
@@ -5,7 +5,7 @@ import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
 import {elements} from './styles.js'
-// import {modifiers as genericsModifiers} from '@mailobj-browser/components-generics/js/styles.js'
+// import {modifiers as genericsModifiers} from '@mailobj-browser/utilities/js/styles.js'
 
 const onClick = object(listener, {
   type: click,
