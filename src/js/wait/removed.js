a27af5320bd2e11c576ff5a39087db8ac636b5b3
Michael Rouges <m.rouges@mailo.com>
2023-05-03 17:37:01 +0200

    restore the focus on lightbox opener when closed

--- src/js/wait/removed.js
+++ src/js/wait/removed.js
@@ -11,7 +11,10 @@ const options = {
 const callback = (records, observer) => {
   for (const { removedNodes } of records) {
     for (const node of removedNodes) {
-      resolvers.get(node)?.(node)
+      console.log({ node })
+      if (observers.get(node.getRootNode()) !== observer) {
+        resolvers.get(node)?.(node)
+      }
     }
   }
 }
