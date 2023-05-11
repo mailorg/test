845d3879e50c6dd630bdf4d3be54e932dc4d6b77
francois <fbouhours@mailobject.com>
2023-05-11 16:39:48 +0200

    Chat rte

--- src/js/rte/smileys/insert.js
+++ src/js/rte/smileys/insert.js
@@ -19,7 +19,7 @@ const onClick = object(listener, {
 
     if (rte) {
 
-      console.log(contentWindow.tagName);
+      console.log(rte.tagName);
       contentWindow.focus()
 
       // let caret = contentWindow.getSelection().anchorNode
