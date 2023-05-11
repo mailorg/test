ee799dca620d4b5c789b5f59423b5f5ba1d6da5e
francois <fbouhours@mailobject.com>
2023-05-11 16:51:10 +0200

    Chat rte

--- src/js/rte/smileys/insert.js
+++ src/js/rte/smileys/insert.js
@@ -3,7 +3,6 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
 import {close} from '../../lightboxes/menus/menu.js'
-import {globals} from '../../ea/ea.js'
 import * as lightbox from '../../lightboxes/lightbox.js'
 
 const onClick = object(listener, {
@@ -20,32 +19,12 @@ const onClick = object(listener, {
     close()
 
     if (rte) {
-
-      console.log(rte.tagName);
-
-
+      //@todo améliorer cette partie, enlever setTimeout et remplacer execCommand
       setTimeout(() => {
         contentWindow.focus()
       }, 100)
 
       contentDocument.execCommand("InsertImage", false, src)
-
-      // let caret = contentWindow.getSelection().anchorNode
-
-      // console.log("1")
-      // const {body} = contentDocument
-      // let rteValue = `<img alt="" src="${src}">`
-      // rte.focus()
-      // contentDocument.open()
-      // contentDocument.write('0123456789')
-      // contentDocument.getSelection().collapseToStart()
-      // contentDocument.close()
-
-      // globals.ea_rte_exec_insert_link(rte.id, src, 'image')
-
-
-
-      // rte.focus()
     }
   }
 })
