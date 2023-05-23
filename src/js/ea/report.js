bc989f5b4ce00f32a48925ad4e77955dd651a8ba
Michael Rouges <m.rouges@mailo.com>
2023-05-23 09:52:20 +0200

    + report.fromEvent(event,  ea_link_file = null)

--- src/js/ea/report.js
+++ src/js/ea/report.js
@@ -4,12 +4,12 @@ import object from '@mailobj-browser/front/js/utils/object.js'
 import href from './href.js'
 
 const report = async param => {
-  console.error(param)
   const { window } = defaults
   const { FormData, Request } = window
   const url = href('util/log')
   const body = new FormData()
   
+  console.error(param)
   body.append('fct', 'JS_LOG')
   body.append(`param_0`, param)
 
