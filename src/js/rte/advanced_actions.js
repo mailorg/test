3508c9fce3ae6158d6d2aaa604eda5d162b9cabf
francois <fbouhours@mailobject.com>
2023-06-12 17:02:54 +0200

    1209818287 - Send

--- src/js/rte/advanced_actions.js
+++ src/js/rte/advanced_actions.js
@@ -1,6 +1,5 @@
 import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
-// import touchEnd from '@mailobj-browser/front/js/events/types/touchEnd.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import one from '@mailobj-browser/front/js/selectors/one.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
