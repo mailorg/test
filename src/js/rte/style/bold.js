738df872145eb48b132ce72ffc1109e70024def6
francois <fbouhours@mailobject.com>
2023-07-06 11:37:34 +0200

    1207548526 - Send

--- src/js/rte/style/bold.js
+++ src/js/rte/style/bold.js
@@ -2,14 +2,15 @@ import listener from '@mailobj-browser/front/js/events/listeners/listener.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
 import click from '@mailobj-browser/front/js/events/types/click.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
-import globals from '../../ea/globals.js'
+import one from '@mailobj-browser/front/js/selectors/one.js'
 import rte from '../rte.js'
 
 const onClick = object(listener, {
   type: click,
   hooks: [preventDefault],
   task(button) {
-    const iframe = button.closest('iframe')
+    const iframe = one('iframe', button.closest('form'))
+
     const {contentWindow, contentDocument} = iframe
     let {body} = contentDocument
     // globals.ea_rte_command('id_msg_text', 'bold', '')
