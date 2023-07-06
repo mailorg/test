a6068d0b4afb0dbab8a609e7ccdead41dfc5fd56
francois <fbouhours@mailobject.com>
2023-07-06 12:26:47 +0200

    1207548526 - Send

--- src/js/rte/style/bold.js
+++ src/js/rte/style/bold.js
@@ -16,7 +16,8 @@ const onClick = object(listener, {
     const userSelection = contentWindow.getSelection()
     const {nextElementSibling} = userSelection
     const selectedTextRange = userSelection.getRangeAt(0)
-
+    const boldNode = one('b', selectedTextRange)
+    console.log(boldNode);
     // console.log(userSelection, selectedTextRange, boldElement);
     if (userSelection.containsNode(boldElement)) {
       console.log(boldElement)
