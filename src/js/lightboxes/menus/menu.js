c36c444601787e60b2905bc7c0d3e6f6a7ffde26
Michael Rouges <m.rouges@mailo.com>
2023-07-20 18:00:57 +0200

    debug

--- src/js/lightboxes/menus/menu.js
+++ src/js/lightboxes/menus/menu.js
@@ -52,6 +52,7 @@ const onFocusIn = object(listener, {
   once,
   passive,
   task: (document, { target }) => {
+    console.log({ focusIn })
     focusing = target
   }
 })
@@ -66,13 +67,16 @@ const onFocusOut = object(listener, {
     const { requestAnimationFrame } = defaultView
     
     focusing = null
+    console.log({ focusOut })
     onFocusIn.listen(ownerDocument)
     requestAnimationFrame(autoClose)
   }
 })
 
 const autoClose = () => {
+  console.log({ autoClose, current, focusing })
   if (current && (!focusing || !current.contains(focusing))) {
+    console.log({ close })
     close()
   }
 }
