cd841e61bb8979743afd4cb325fa29e445cac7cf
Michael Rouges <m.rouges@mailo.com>
2023-09-08 07:38:03 +0200

    debug*

--- src/js/lightboxes/menus/menu.js
+++ src/js/lightboxes/menus/menu.js
@@ -26,6 +26,7 @@ export const { focus, opener } = lightbox
 
 export const close = () => {
   if (current) {
+    console.log('close')
     onOpenerTapUp.forget(opener(current))
     remove(current)
     current = null
