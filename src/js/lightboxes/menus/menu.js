3390c4e766d73f641871874a6ee3267bb7f12bba
Michael Rouges <m.rouges@mailo.com>
2023-09-14 15:54:26 +0200

    debug

--- src/js/lightboxes/menus/menu.js
+++ src/js/lightboxes/menus/menu.js
@@ -75,6 +75,7 @@ const onFocusOut = object(listener, {
   capture,
   passive,
   task: async (list, { relatedTarget }) => {
+    alert(relatedTarget)
     if (!relatedTarget || relatedTarget !== opener(list)) {
       requestAnimationFrame(close)
     }
