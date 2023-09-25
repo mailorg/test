d5ee5a07c635975bed93eecbf237c740f5280494
Lucie Rouges <l.rouges@mailo.com>
2023-09-25 08:14:43 +0200

    debug

--- src/js/lightboxes/menus/menu.js
+++ src/js/lightboxes/menus/menu.js
@@ -86,7 +86,7 @@ const onFocusOut = object(listener, {
     }
     
     if (relatedTarget !== opener(list)) {
-      requestAnimationFrame(close)
+      //requestAnimationFrame(close)
     }
   }
 })
