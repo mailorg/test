ae33e3f9a4e7c3957a0f82b9f76c562ee1b137db
Lucie Rouges <l.rouges@mailo.com>
2023-10-19 08:55:38 +0200

    + ea.confirm.fromOpener

--- src/js/ea/confirm.js
+++ src/js/ea/confirm.js
@@ -13,6 +13,7 @@ import manager from '@mailobj-browser/front/js/contracts/manager.js'
 import all from '@mailobj-browser/front/js/selectors/all.js'
 import resolvable from '@mailobj-browser/front/js/utils/resolvable.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
+import * as menu from '../lightboxes/menus/menu.js'
 
 const resolvers = new WeakMap()
 
@@ -22,13 +23,13 @@ const onClick = object(listener, {
   passive,
   task: button => {
     const { value } = button
-
+    
     remove(button.closest('div.ea_generics__modal'))
     resolvers.get(button)?.(value === '1')
   }
 })
 
-export default async (
+const confirm = async (
   title,
   text = '',
   openers = []
@@ -37,30 +38,30 @@ export default async (
   const template = one(`.${utilities.elements.aside_confirms} template`, root)
   const { content, parentNode } = template
   const [promise, { resolve }] = resolvable()
-
+  
   const confirm = one('div.ea_generics__modal', content).cloneNode(true)
   const dialog = one('dialog', confirm)
   const header = one('div.ea_generics__modal_header', dialog)
   const h2 = one('h2', header)
-
+  
   const body = one('div.ea_generics__modal_main', dialog)
   const paragraph = one('p', body)
-
+  
   const footer = one('div.ea_generics__modal_footer', dialog)
   const buttons = all('button', footer)
   const [focusable] = buttons
-
+  
   append(h2, title)
   
   if (text.length) {
     append(paragraph, text)
   }
-
+  
   for (const button of buttons) {
     resolvers.set(button, resolve)
     onClick.listen(button)
   }
-
+  
   await manager.fragment(confirm)
   replaceChildren(parentNode, template, confirm)
   focusable.focus()
@@ -80,3 +81,30 @@ export default async (
   
   return result === true
 }
+
+export const fromOpener = async opener => {
+  const { dataset } = opener
+  const { confirm: message } = dataset
+  
+  if (!message) {
+    return true
+  }
+  
+  const { title, text = ''} = JSON.parse(message)
+  const openers = [opener]
+  const list = menu.list(opener)
+  
+  if (list) {
+    openers.push(menu.opener(list))
+  }
+  
+  const result = await confirm(title, text, openers)
+  
+  if (list) {
+    menu.close()
+  }
+  
+  return result
+}
+
+export default confirm
