ab68d708415eb97d6eeb32e39d229d235931fabf
Lucie Rouges <l.rouges@mailo.com>
2024-01-17 12:29:25 +0100

    fixes missing .js extension

--- src/js/hotkey/modal.js
+++ src/js/hotkey/modal.js
@@ -1,7 +1,7 @@
 import { hotkey } from '@mailobj-browser/components-generics/js/hotkey/hotkey.js'
 import object from '@mailobj-browser/front/js/utils/object.js'
 import preventDefault from '@mailobj-browser/front/js/events/hooks/preventDefault.js'
-import { onTapUp } from '@mailobj-browser/components-generics/js/selectors/templates/anchor'
+import { onTapUp } from '@mailobj-browser/components-generics/js/selectors/templates/anchor.js'
 
 const handler = object(null, {
     async task(
